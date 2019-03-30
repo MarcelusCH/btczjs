ba0f20897f624ee3a9fa2eaf308a429a115bf396
renuzit <renuzit@btcz.life>
2019-03-30 18:23:04 -0500

    Adding params for BitcoinZ

--- src/config.js
+++ src/config.js
@@ -4,22 +4,27 @@ config.js - Configuration for ZENCash Coin
 
 module.exports = {
   mainnet: {
-    messagePrefix: 'ZENCash main net',
+    messagePrefix: '\x18BitcoinZ Signed Message:\n',
     bip32: {
-      public: '0488b21e',
-      private: '0488ade4'
+        public: '0488b21e',
+        private: '05358394'
     },
-    pubKeyHash: '2089',
-    scriptHash: '2096',
+    pubKeyHash: '1cb8',
+    scriptHash: '1cbd',
     zcPaymentAddressHash: '169a', // Private z-address
     zcSpendingKeyHash: 'ab36', // Spending key
     wif: '80'
   },
   testnet: {
-    wif: 'ef',
-    pubKeyHash: '2098',
-    scriptHash: '2092',
+    messagePrefix: '\x18BitcoinZ Signed Message:\n',
+    bip32: {
+      public: '043587cf',
+      private: '04358394'
+    },
+    pubKeyHash: '1d25',
+    scriptHash: '1cba',
     zcPaymentAddressHash: '16b6',
-    zcSpendingKeyHash: 'ac08'
+    zcSpendingKeyHash: 'ac08',
+    wif: 'ef'
   }
-}
+};
